7660086030318df9e965e1551c505c7d448fc79e
Vincent H <hoangvincent69@gmail.com>
2021-09-29 17:51:41 -0700

    Inital Commit

---
+++ lib/SignIn.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'SignUp.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In")
+      ),
+      body: Column(
+        children: [
+          Text(
+            'Welcome to Room.me',
+             style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top:100, bottom:50),
+            child: Text(
+              'Sign In',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+            ),
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: (){
+                //todo code
+              },
+              child: Text(
+                'Sign In'
+              ),
+          ),
+          RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                text: 'Already have account? ',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              TextSpan(
+                  text: 'Sign Up',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp(title: 'Room.me',)),
+                      );;
+                    }),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/SignUp.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'SignIn.dart';
+
+class SignUp extends StatefulWidget {
+  SignUp({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Sign Up")
+      ),
+      body: Column(
+        children: [
+          Text(
+            'Welcome to Room.me',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top:100, bottom:50),
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              //todo code
+            },
+            child: Text(
+                'Sign Up'
+            ),
+          ),
+          RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                text: 'Already have account? ',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              TextSpan(
+                  text: 'Sign In',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignIn(title: 'Room.me',)),
+                      );;
+                    }),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
